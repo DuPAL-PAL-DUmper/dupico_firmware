50b5fa42941f066896a9c0a0ac169799e13a0117
Fabio Battaglia <tabaglio@posteo.net>
2024-03-25 21:16:22 +0100

    add task code to handle shifter i/o

--- src/tasks/shifter_io_task.c
+++ src/tasks/shifter_io_task.c
@@ -1,1 +1,31 @@
 #include "shifter_io_task.h"
+
+#include <portmacro.h>
+
+#include <stdbool.h>
+
+void shifter_io_task(void *params) {
+    shifter_io_task_params *prms = (shifter_io_task_params*)params;
+    shifter_io_task_cmd cmd;
+    bool keep_going = true;
+    uint64_t val;
+
+    while(keep_going) {
+        if(xQueueReceive(prms->cmd_queue, (void*)&cmd, portMAX_DELAY)) {
+            switch(cmd.cmd) {
+                case WRITE:
+                    sipo_shifter_set(&(prms->sipo_cfg), cmd.param);
+                case READ:
+                    val = piso_shifter_get(&(prms->piso_cfg));
+                    xQueueSend(prms->resp_queue, (void*)&val, portMAX_DELAY);
+                    break;
+                case DIE:
+                default:
+                    keep_going = false;
+                    break;
+            }
+        }
+    }
+
+    vTaskDelete(NULL);
+}
--- src/tasks/shifter_io_task.h
+++ src/tasks/shifter_io_task.h
@@ -3,15 +3,29 @@
 
 #include <stdint.h>
 
+#include <hardware/shifters/piso_shifter.h>
+#include <hardware/shifters/sipo_shifter.h>
+
+#include <queue.h>
+
 typedef enum {
     READ,
-    WRITE
-} shf_io_cmd_type;
+    WRITE,
+    DIE
+} shifter_io_task_cmd_type;
 
 typedef struct {
-    shf_io_cmd_type cmd;
+    shifter_io_task_cmd_type cmd;
     uint64_t param;
-} shf_io_cmd;
+} shifter_io_task_cmd;
+
+typedef struct {
+    PISO_Config piso_cfg;
+    SIPO_Config sipo_cfg;
+
+    QueueHandle_t cmd_queue;
+    QueueHandle_t resp_queue;
+} shifter_io_task_params;
 
 void shifter_io_task(void *params);
 
