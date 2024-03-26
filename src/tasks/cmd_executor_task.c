7572c7c6f72d77b7bb79a6732875ade298d0354a
Fabio Battaglia <tabaglio@posteo.net>
2024-03-26 20:55:54 +0100

    begin defining command structure

--- src/tasks/cmd_executor_task.c
+++ src/tasks/cmd_executor_task.c
@@ -1,5 +1,39 @@
 #include "cmd_executor_task.h"
 
+void dummy_command(QueueHandle_t resp_queue, void *params);
+
+typedef void(*cmd_func)(QueueHandle_t, void*);
+
+static cmd_func command_map[] = {
+    dummy_command, // READ
+    dummy_command, // WRITE
+    dummy_command, // GET_POWER
+    dummy_command, // SET_POWER
+    dummy_command, // DUMP_TO_SD
+    dummy_command, // STREAM_FROM_SD
+};
+
+void dummy_command(QueueHandle_t resp_queue, void *params) {
+    return;
+}
+
 void cmd_executor_task(void *params) {
+    cmd_executor_task_params *prms = (cmd_executor_task_params*)params;
+    cmd_executor_command cmd;
+    bool keep_going = true;
+
+    while(keep_going) {
+        if(xQueueReceive(prms->cmd_queue, (void*)&cmd, portMAX_DELAY)) {
+            switch(cmd.cmd) {
+                case DIE:
+                    keep_going = false;
+                    break;
+                default:
+                    command_map[cmd.cmd](prms->resp_queue, NULL); // TODO: Add some kind of parameter
+                    break;
+            }
+        }
+    }
+
     vTaskDelete(NULL);
 }
--- src/tasks/cmd_executor_task.h
+++ src/tasks/cmd_executor_task.h
@@ -4,6 +4,21 @@
 #include <FreeRTOS.h>
 #include <queue.h>
 
+typedef enum {
+    READ = 0,
+    WRITE = 1,
+    GET_POWER = 2,
+    SET_POWER = 3,
+    DUMP_TO_SD = 4,
+    STREAM_FROM_SD = 5,
+    DIE
+} cmd_executor_command_type;
+
+typedef struct {
+    const cmd_executor_command_type cmd;
+    const void *param;
+} cmd_executor_command;
+
 typedef struct {
     const QueueHandle_t cmd_queue;
     const QueueHandle_t resp_queue;
