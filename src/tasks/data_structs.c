94d2fdb6223a92e456accbb35aaafdb810a79ed0
tabaglio <tabaglio@posteo.net>
2024-03-28 21:30:42 +0100

    begin defining functions to implement

--- src/tasks/data_structs.c
+++ src/tasks/data_structs.c
@@ -1,5 +1,5 @@
 #include "data_structs.h"
 
 uint calculate_IC_Ctrl_Struct_size(IC_Ctrl_Struct *iccd) {
-    return sizeof(IC_Ctrl_Struct) + (iccd->address_len) + (iccd->data_len) + (iccd->power_len) + (iccd->special_len*2);
+    return sizeof(IC_Ctrl_Struct) + (iccd->i_len) + (iccd->io_len) + (iccd->pwr_len) + (iccd->ctrl_len*2);
 }
--- src/tasks/data_structs.h
+++ src/tasks/data_structs.h
@@ -7,20 +7,26 @@
 typedef struct __attribute__((packed)) {
     char name[16];
     uint16_t chip_type;
-    uint8_t address_len;
-    uint8_t address_idx;
-    uint8_t data_len;
-    uint8_t data_idx;
-    uint8_t power_len;
-    uint8_t power_idx;
-    uint8_t special_len;
-    uint8_t special_map_idx;
-    uint8_t special_type_idx;
+    uint8_t i_len;
+    uint8_t i_idx;
+    uint8_t io_len;
+    uint8_t io_idx;
+    uint8_t pwr_len;
+    uint8_t pwr_idx;
+    uint8_t ctrl_len;
+    uint8_t ctrl_map_idx;
+    uint8_t ctrl_type_idx;
     uint8_t data[];
     // Where data is 
-    // address_len + data_len + power_len + (special_len * 2) long
+    // i_len + io_len + pwr_len + (ctrl_len * 2) long
 } IC_Ctrl_Struct;
 
 uint calculate_IC_Ctrl_Struct_size(IC_Ctrl_Struct *iccd);
+
+uint64_t ctrl_struct_i_to_mask(IC_Ctrl_Struct *iccd, uint64_t i);
+uint64_t ctrl_struct_io_to_mask(IC_Ctrl_Struct *iccd, uint64_t io);
+uint64_t ctrl_struct_mask_to_io(IC_Ctrl_Struct *iccd, uint64_t mask);
+uint64_t ctrl_struct_pwr_to_mask(IC_Ctrl_Struct *iccd, uint64_t pwr);
+uint64_t ctrl_struct_ctrl_to_mask(IC_Ctrl_Struct *iccd, uint64_t ctrl);
 
 #endif /* _DATA_STRUCTS_HEADER_ */
