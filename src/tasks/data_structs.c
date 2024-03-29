092f67c71c218a57ec8387759e757e7da6373e95
Fabio Battaglia <tabaglio@posteo.net>
2024-03-29 09:51:01 +0100

    complete first version of mapping functions

--- src/tasks/data_structs.c
+++ src/tasks/data_structs.c
@@ -3,6 +3,7 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 static inline uint64_t map_to_mask_conversion(uint32_t src, uint8_t max_len, uint8_t *map);
+static inline uint32_t mask_to_map_conversion(uint64_t mask, uint8_t max_len, uint8_t *map);
 
 static inline uint64_t map_to_mask_conversion(uint32_t src, uint8_t max_len, uint8_t *map) {
     uint64_t mask = 0;
@@ -14,6 +15,16 @@ static inline uint64_t map_to_mask_conversion(uint32_t src, uint8_t max_len, uin
     return mask;
 }
 
+static inline uint32_t mask_to_map_conversion(uint64_t mask, uint8_t max_len, uint8_t *map) {
+    uint32_t data = 0;
+
+    for(uint8_t idx = 0; idx < max_len; idx++) {
+        data |= (mask & (1 << map[idx])) ? (1 << idx) : 0;
+    }
+
+    return data;
+}
+
 uint64_t ctrl_struct_i_to_mask(IC_Ctrl_Struct *iccd, uint32_t i) {
     return map_to_mask_conversion(i, MIN(iccd->i_len, MAX_I_LEN), iccd->i_map);
 }
@@ -23,7 +34,7 @@ uint64_t ctrl_struct_io_to_mask(IC_Ctrl_Struct *iccd, uint16_t io) {
 }
 
 uint16_t ctrl_struct_mask_to_io(IC_Ctrl_Struct *iccd, uint64_t mask) {
-    return 0;
+    return mask_to_map_conversion(mask, MIN(iccd->io_len, MAX_IO_LEN), iccd->io_map) & 0xFFFF;
 }
 
 uint64_t ctrl_struct_pwr_to_mask(IC_Ctrl_Struct *iccd, uint8_t pwr) {
