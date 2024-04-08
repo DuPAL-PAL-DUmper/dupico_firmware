8de8b2d9d4ebab2c68ee23b75232d01456428459
Fabio Battaglia <tabaglio@posteo.net>
2024-04-08 21:13:18 +0200

    forgot an header

--- src/tasks/command_hub_task.h
+++ src/tasks/command_hub_task.h
@@ -1,6 +1,14 @@
 #ifndef _COMMAND_HUB_TASK_
 #define _COMMAND_HUB_TASK_
 
+#include <FreeRTOS.h>
+
+typedef struct {
+} command_hub_command;
+
+typedef struct {
+} command_hub_command_response;
+
 void command_hub_task(void *params);
 
 #endif /* _COMMAND_HUB_TASK_ */
