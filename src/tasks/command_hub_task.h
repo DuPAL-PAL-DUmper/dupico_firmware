f33d044bc02ad768cacf987f1eea264b0687dd71
Fabio Battaglia <tabaglio@posteo.net>
2024-04-10 19:04:50 +0200

    include reset command for the command hub

--- src/tasks/command_hub_task.h
+++ src/tasks/command_hub_task.h
@@ -1,6 +1,9 @@
 #ifndef _COMMAND_HUB_TASK_
 #define _COMMAND_HUB_TASK_
 
+#include <tasks/data_structs.h>
+#include <tasks/ic_handlers/ic_handlers.h>
+
 #include <FreeRTOS.h>
 #include <queue.h>
 
@@ -11,8 +14,20 @@ typedef enum {
     CMDH_SUPPORTED_IC_LIST_PREV,
     CMDH_SUPPORTED_IC_LIST_SELECT,
     CMDH_SELECTED_IC_GET_CMD_LIST,
-    CMDH_SELECTED_IC_EXEC_CMD
+    CMDH_SELECTED_IC_EXEC_CMD,
+    CMDH_RESET
 } command_hub_cmd_type;
+
+typedef struct {
+    cmd_list_entry *cmds;
+    uint size;
+} command_hub_cmd_resp_cmdlist;
+
+typedef union {
+    command_hub_cmd_resp_cmdlist cmdlist;
+    IC_Ctrl_Struct iccd;
+    uint32_t data;
+} command_hub_cmd_resp_data;
 
 typedef struct {
     command_hub_cmd_type type;
@@ -20,7 +35,9 @@ typedef struct {
 } command_hub_cmd;
 
 typedef struct {
+    cmd_list_entry cmd;
     uint id;
+    command_hub_cmd_resp_data data;
 } command_hub_cmd_resp;
 
 typedef struct {
